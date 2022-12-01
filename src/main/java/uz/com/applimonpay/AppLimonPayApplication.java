6b94d1f684c6dcedf02cac8ae5be9aabda7ff862
Sherkhan <sherzodbektatu@gmail.com>
2022-12-01 15:36:08 +0500

    First initial commit

---
+++ src/main/java/uz/com/applimonpay/AppLimonPayApplication.java
@@ -0,0 +1,13 @@
+package uz.com.applimonpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLimonPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppLimonPayApplication.class, args);
+    }
+
+}
